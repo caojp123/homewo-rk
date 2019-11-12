c8cb6f00ba77b30fce3bc7d287d89bf0dee8cd92
caojianping <630321721@qq.com>
2019-11-12 08:58:51 +0800

    caodaye

---
+++ app1-master/app1/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+
+def test(request):
+    return HttpResponse("enguangshagou")
+
+def index(request):
+    print ("小许真TM的帅")
+    return HttpResponse()
+
+def index2(request):
+
+    print ("燕恩光是个大弱智！！！")
+    return HttpResponse()
+
+def index3(request):
+    print("这是第三个index")
+
+    return HttpResponse()
+
+def index3(request):
+    print("ccc123")
+    return HttpResponse()
